8435ce3b1e94e82e67466833b0da916f6fe4905f
Madan <58905767+Madanaa@users.noreply.github.com>
2026-02-14 17:50:07 +0530

    Add SomOfProducts.java to calculate sum and product

---
+++ JavaBasic/day1/SomOfProducts.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 100, b = 8, c = 3;
+
+        int sum = a + b + c;
+        int product = a * b * c;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Product = " + product);
+    }
+}
