dc241cde863a7f87e0c3934cc290e23a825d0669
Madan <58905767+Madanaa@users.noreply.github.com>
2026-02-14 17:34:20 +0530

    Create PrintString.java to print messages

---
+++ JavaBasic/day1/PrintString.java
@@ -0,0 +1,10 @@
+package javabasic1week;
+
+public class PrintString {
+    public static void main(String[] args) {
+        System.out.println("I");
+        System.out.println("Am");
+        System.out.println("Enjoying");
+        System.out.println("Coding");
+    }
+}
