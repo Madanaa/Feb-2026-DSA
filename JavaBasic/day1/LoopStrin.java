e9caea8827949c821a9c4e7549cc03cccc8432a0
Madan <58905767+Madanaa@users.noreply.github.com>
2026-02-14 17:41:28 +0530

    Add LoopString class to print array elements

---
+++ JavaBasic/day1/LoopStrin.java
@@ -0,0 +1,11 @@
+package javabasic1week;
+
+public class LoopString {
+    public static void main(String[] args) {
+        String a[] = {"My","Name","Madan"};
+
+        for (String aa : a) {
+            System.out.println(aa);
+        }
+    }
+}
